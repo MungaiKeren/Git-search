d9f414dc52504a107e7cdd1154b72aec0ac3be54
MungaiKeren <wambukeren@gmail.com>
2019-08-23 15:45:24 +0300

    This commit will create a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchFormComponent
   ],
   imports: [
     BrowserModule,
