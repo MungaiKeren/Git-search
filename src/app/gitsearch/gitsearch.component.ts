9ae8dc45d7219fbbef44698b1a1e9b2ec00aabe5
MungaiKeren <wambukeren@gmail.com>
2019-08-24 13:12:45 +0300

    This commit will display the user profile image

--- src/app/gitsearch/gitsearch.component.ts
+++ src/app/gitsearch/gitsearch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpServiceService } from '../http-service/http-service.service';
+import { User } from '../user/user';
 
 @Component({
   selector: 'app-gitsearch',
@@ -8,12 +9,22 @@ import { HttpServiceService } from '../http-service/http-service.service';
 })
 export class GitsearchComponent implements OnInit {
 
+    user:User[];
  
-  constructor(kay:HttpServiceService) { }
+  constructor(private kay:HttpServiceService) { }
 
   ngOnInit() {
+    this.kay.keren('MungaiKeren');
   }
  name(userName){
-    console.log(userName)
+    this.kay.keren(userName).then(
+      (success)=>{
+        this.user=this.kay.myUser;
+        console.log(this.user)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
   }
 }
--- src/app/http-service/http-service.service.ts
+++ src/app/http-service/http-service.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { User } from '../user/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
+  myUser:User [] =[];
 
   url = 'https://api.github.com/users/';
   token = '?access_token=' + environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
-
-
   keren(userName:string){
+    interface data {
+      login:string;
+    }
     let promise =  new Promise ((resolve, reject)=>{
-      this.http.get(this.url+userName+this.token).toPromise().then(
+      this.myUser = []
+      this.http.get<data>(this.url+userName+this.token).toPromise().then(
         (result)=>{
-          console.log(result)
+          this.myUser.push(result);
           resolve();
         },
         (error)=>{
