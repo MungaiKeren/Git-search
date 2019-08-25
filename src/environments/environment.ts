a3785d20561192b8e0ff69cc6f0c0c4b3e03fa0d
MungaiKeren <wambukeren@gmail.com>
2019-08-25 23:51:46 +0300

    change production type to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   APIKEY:'920f89928c0d42223832f040243f5730e0b7c87b',
 };
 
